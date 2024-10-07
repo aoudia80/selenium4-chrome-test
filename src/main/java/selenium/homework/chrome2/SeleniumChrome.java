df21f4524e3fa310c0cef50fca574ab95557c04b
aksil <aoudia80@yahoo.fr>
2024-10-06 22:46:00 -0400

    Project 4 open home depot with chrome using selenium4

--- src/main/java/selenium/homework/chrome2/SeleniumChrome.java
+++ src/main/java/selenium/homework/chrome2/SeleniumChrome.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumChrome {
     public static void main(String[] args) {
 
-        // Create a new instance of the Chrome browser using the WebDriver interface selenium 4
+        // Create a new instance of the Chrome browser using the WebDriver interface selenium 4git
         WebDriver driver = new ChromeDriver();
 
         // Navigate to Home Depot's website
